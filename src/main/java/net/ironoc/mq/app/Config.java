e08dd29bf3a878d83bd17a5c778aa28a86aa0e82
Conor H <conor.heffron@gmail.com>
2025-11-10 12:39:41 +0000

    Migrate to Java 17, Add build & main run profiles

--- src/main/java/net/ironoc/mq/app/Config.java
+++ src/main/java/net/ironoc/mq/app/Config.java
@@ -1,12 +1,15 @@
-package com.ironoc.mq.app;
+package net.ironoc.mq.app;
 
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
+import java.net.URI;
+
 public class Config {
 	
-	public Connection getConnection(String uri) {
-		ConnectionFactory factory = new ConnectionFactory();
+	public Connection getConnection(URI uri) {
+        ConnectionFactory factory = new ConnectionFactory();
+
 		factory.setRequestedHeartbeat(30);
 		factory.setConnectionTimeout(60000);
 		Connection connection = null;
